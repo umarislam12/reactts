bffb44af8cf34ae5fc11998f30bd431f1963f6f0
obnox <obnox@DESKTOP-KOIF7PI>
2021-10-10 23:16:31 +0500

    added for control values

--- src/Components/Home/counter.tsx
+++ src/Components/Home/counter.tsx
@@ -1,16 +1,21 @@
+// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import { Calculator } from './temperatureCalculator';
 import PT from 'prop-types'
 export default function Example() {
-  const [count, setCount] = useState(0);
- 
+  let [count, setCount] = useState(0);
+  
   
   useEffect(() => {
+    const counter=localStorage.getItem("count");
+    if(counter!==null){
+      count=parseInt(count)
+    }
     if(count===10){
       throw Error
     }
-    console.log(document.title = `You  ${count} times which is less than 5`);
-    
+    console.log(document.title = `You  ${count} times which is less than 10`);
+   
   },[count]);
   
   return (
