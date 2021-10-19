7e6efe8cea1097a90e087c53a52d645fe95238c7
obnox <obnox@DESKTOP-KOIF7PI>
2021-10-19 15:13:00 +0500

    added new field in exchange

--- src/Components/CurrencyExchange/CurrencyExchange.tsx
+++ src/Components/CurrencyExchange/CurrencyExchange.tsx
@@ -1,68 +1,80 @@
 // @ts-nocheck
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const CurrencyExchange = () => {
-  const currencies = ["PKR", "CNY", "AED", "AMD"];
-  const [base, setBase] = useState("EUR");
-  const [other, setOther] = useState("PKR");
-  const [value, setValue] = useState(0);
-  const [converted, setConverted] = useState(0);
-//   const handleInputChange =  (e) => {
-//     /// console.log(e.target.value)
-//    setValue(e.target.value);
-//     recalculate();
-//   };
-useEffect(() => {
+    const currencies = ["PKR", "CNY", "AED", "AMD"];
+    const [base, setBase] = useState("EUR");
+    const [other, setOther] = useState("PKR");
+    const [value, setValue] = useState(0);
+    const [converted, setConverted] = useState(0);
+    //   const handleInputChange =  (e) => {
+    //     /// console.log(e.target.value)
+    //    setValue(e.target.value);
+    //     recalculate();
+    //   };
+    useEffect(() => {
 
- recalculate()
-}, [value,base,other])
+        recalculate()
+    }, [value, base, other])
 
-  const recalculate = () => {
-    const cal = parseFloat(value);
-    console.log(cal)
-    if (isNaN(cal)) {
-      return;
-    }
-    // http://api.currencylayer.com/live?access_key=b113dd3aad838a617158f452c59b7bef& source = USD
-    fetch(`http://data.fixer.io/api/latest?access_key=22109a26c619daf629f0fdee4aa0c806&format=1`)
-      .then((response) => response.json())
-      .then((data) => {
-        setConverted(data.rates[other] * cal);
-      }).catch((error)=>{console.log(error)})
-      
-  };
-  return (
-    <div>
-      <div>
-        <select
-          onChange={(e) => setBase(e.target.value)}
-          name="base"
-          value={base}
-        >
-       
-            <option value={base}>
-              {base}
-            </option>
-          
-        </select>
-        <input type="number" onChange={(e) =>setValue(parseFloat(e.target.value)) && setConverted(null)&&console.log(converted)} value={value} />
-      </div>
-      <div>
-        <select
-          onChange={(e) => setOther(e.target.value)}
-          name="other"
-          value={other}
-        >
-          {currencies.map((currency) => (
-            <option key={currency} value={currency}>
-              {currency}
-            </option>
-          ))}
-        </select>
-        <input  disabled={true} value={converted===null?"calculating..":converted} />
-      </div>
-    </div>
-  );
+    const recalculate = () => {
+        const cal = parseFloat(value);
+        console.log(cal)
+        if (isNaN(cal)) {
+            return;
+        }
+        // http://api.currencylayer.com/live?access_key=b113dd3aad838a617158f452c59b7bef& source = USD
+        fetch(`http://data.fixer.io/api/latest?access_key=22109a26c619daf629f0fdee4aa0c806&format=1`)
+            .then((response) => response.json())
+            .then((data) => {
+                setConverted(data.rates[other] * cal);
+            }).catch((error) => { console.log(error) })
+
+    };
+    return (
+        <div>
+            <div>
+                <select
+                    onChange={(e) => setBase(e.target.value)}
+                    name="base"
+                    value={base}
+                >
+
+                    <option value={base}>
+                        {base}
+                    </option>
+
+                </select>
+                <input type="number" onChange={(e) => setValue(parseFloat(e.target.value)) && setConverted(null) && console.log(converted)} value={value} />
+            </div>
+            <div>
+                <select
+                    onChange={(e) => setOther(e.target.value)}
+                    name="other"
+                    value={other}
+                >
+                    {currencies.map((currency) => (
+                        <option key={currency} value={currency}>
+                            {currency}
+                        </option>
+                    ))}
+                </select>
+                <input disabled={true} value={converted === null ? "calculating.." : converted} />
+            </div>
+            <div>
+
+            
+            <input name="country" list="countries" />
+                <datalist id="countries">
+                    <option value="afghanistan"></option>
+                    <option value="Pakistan"></option>
+                    <option value="Armenia"></option>
+                    <option value="Austrlia"></option>
+                </datalist>
+            
+            </div>
+        </div>
+    );
 };
 
 export default CurrencyExchange;
